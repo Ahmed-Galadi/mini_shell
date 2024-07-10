4dcdc0307db4e6aa359390e1472fac43aba367a0
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-07-10 03:44:34 +0100

    ...

--- main.c
+++ main.c
@@ -23,15 +23,39 @@ int		quotes_count(char *str)
 
 }
 
-char	*check_pipe_in_quote(char *str)
+void	check_pipe_in_quote(char *str)
 {
+	char	*tmp;
+	int		i;
+
 	if ((quotes_count(str) % 2) != 0)
 		error();
-	
+	tmp = str;
+	i = 0;
+	while (tmp[i] && tmp[i] != '\"')
+		i++;
+	while (tmp[i] && tmp[i] == '\"')
+		i++;
+	while (tmp[i])
+	{
+		if (tmp[i] == '|')
+		{
+			tmp[i] = (char)-1;
+			tmp = tmp + i + 1;
+			i = 0;
+		}
+		if (tmp[i] == '\"')
+			break;
+		i++;
+	}
 }
 
 int main(int argc, char *argv[])
 {
-	
+	char str[] = "cat -e |  \"\"\"mel' | ody\"\"\" | ";
+	printf("%s\n", str);
+	check_pipe_in_quote(str);
+	printf("%s\n", str);
+
 	return (0);
 }
